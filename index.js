0352d757e8ece90931dcd514e50ccb42b6738207
zooeymiller <zooeyxmiller@gmail.com>
2018-05-23 10:35:35 +0100

    have commented out the functions that made everything break

--- index.js
+++ index.js
@@ -7,54 +7,62 @@ const person = {
 };
 
 // two
+// uncomment from here to 'three' when done with one
+// and 'catGreeting' from the export
 
-class Cat {
-  constructor(name) {
-    this.name = name;
-  }
+// class Cat {
+//   constructor(name) {
+//     this.name = name;
+//   }
 
-  sayHi() {
-    return `Hi I'm ${this.name}, miaow`;
-  }
-}
+//   sayHi() {
+//     return `Hi I'm ${this.name}, miaow`;
+//   }
+// }
 
-const getCatName = ({ sayHi }) => sayHi();
+// const getCatName = ({ sayHi }) => sayHi();
 
-const catGreeting = getCatName(new Cat("Francois"));
+// const catGreeting = getCatName(new Cat("Francois"));
 
 // three
+// uncomment from here to the export when you're done with two
+// and 'Counter' from the export
 
-const Button = ({ onClick, children }) => (
-  <button onClick={onClick}>{children}</button>
-);
+// const Button = ({ onClick, children }) => (
+//   <button onClick={onClick}>{children}</button>
+// );
 
-class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0
-    };
-  }
+// class Counter extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       count: 0
+//     };
+//   }
 
-  handleUpdateCount(type) {
-    return function() {
-      this.setState(({ count }) => ({
-        count: type === "inc" ? count + 1 : count - 1
-      }));
-    };
-  }
+//   handleUpdateCount(type) {
+//     return function() {
+//       this.setState(({ count }) => ({
+//         count: type === "inc" ? count + 1 : count - 1
+//       }));
+//     };
+//   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Button onClick={this.handleUpdateCount("inc")}>+</Button>
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Button onClick={this.handleUpdateCount("inc")}>+</Button>
 
-        <p data-testid="count">{this.state.count}</p>
+//         <p data-testid="count">{this.state.count}</p>
 
-        <Button onClick={this.handleUpdateCount("dec")}>-</Button>
-      </React.Fragment>
-    );
-  }
-}
+//         <Button onClick={this.handleUpdateCount("dec")}>-</Button>
+//       </React.Fragment>
+//     );
+//   }
+// }
 
-export { person, catGreeting, Counter };
+export {
+  person
+  // catGreeting,
+  // Counter
+};
